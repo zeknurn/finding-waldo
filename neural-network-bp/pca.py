a295e4fae041fb039f379cd9706d14bbed107b75
Viktor Rydsund <62593116+Rydsund@users.noreply.github.com>
2022-09-19 19:29:13 +0200

    Add primitive solution for attribute vector
    Will try to utilize numpy in later commit

--- neural-network-bp/pca.py
+++ neural-network-bp/pca.py
@@ -3,11 +3,32 @@ from os import listdir
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+from PIL import Image
 
 # Waldo
 path = '/home/vic/Downloads/archive/wheres-waldo/Hey-Waldo/64-bw/waldo'
+count = 0
 for file in listdir(path):
-    image = open(path + '/' + file)
+    if count < 1:
+        count += 1
+        image_path = path + '/' + file
+        image = Image.open(image_path)
+        pixels = image.load()
+        width, height = image.size
+
+        # Testing numpy alternative.
+
+        # Converting each image to a set of attribute vectors.
+        attribute_vector = []
+        for y in range(height):
+            pixels_in_row = []
+            for x in range(width):
+                pixels_in_row.append(pixels[x, y])
+            attribute_vector.append(pixels_in_row)
+
+
+        Image.fromarray(pixels)
+
 
 
 # Not waldo
