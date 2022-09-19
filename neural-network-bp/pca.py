8956c96bb85d270f4742e15aab581f6fc1f3ab45
Viktor Rydsund <62593116+Rydsund@users.noreply.github.com>
2022-09-19 12:15:16 +0200

    Add python file for PCA

---
+++ neural-network-bp/pca.py
@@ -0,0 +1,12 @@
+from os import listdir
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+
+
+# Waldo
+for image in listdir('/home/vic/Downloads/archive/wheres-waldo/Hey-Waldo/64-bw/waldo'):
+    print(image)
+
+# Not waldo
