eb8aee0340568d802a083389362949909bdb9b00
Viktor Rydsund <62593116+Rydsund@users.noreply.github.com>
2022-09-21 18:48:34 +0200

    Add working PCA
    Code also contains commented blocks for finding the optimum amount of components.

--- neural-network-bp/pca.py
+++ neural-network-bp/pca.py
@@ -1,34 +1,59 @@
 from os import listdir
-
 import numpy as np
-import matplotlib as plt
-import pandas as pd
-from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.decomposition import PCA
 
 # Waldo
-path = '/home/vic/Downloads/archive/wheres-waldo/Hey-Waldo/64-bw/waldo'
+path = '/home/vic/Downloads/archive/wheres-waldo/Hey-Waldo/64/waldo'
 count = 0
 for file in listdir(path):
     if count < 1:
         count += 1
         image_path = path + '/' + file
 
-        # Convert image to BW using mean.
-        image = Image.open(image_path)
-        h, w = image.size
-        gray = image.convert('L') # Convert to grayscale
-        bw = np.asarray(gray).copy()
-        bw[bw < 128] = 0
-        bw[bw >= 128] = 255
+        rgb_image = plt.imread(image_path)
+        b, g, r = cv2.split(rgb_image)  # RGB are stored in reverse order in OpenCV
+        r_scaled = r / 255
+        g_scaled = g / 255
+        b_scaled = b / 255
 
-        # For testing
-        # bw_image = Image.fromarray(bw)
-        # bw_image.show()
+        # Test PCA
+        #pca_r = PCA(n_components=None) # None, keeps all components. 32, 48 good range?
+        #pca_r.fit(r)
 
-        # Converting pixel values to attribute vector
-        attributes = []
-        for y in range(h):
-            pix_row = []
-            for x in range(w):
-                pix_row.append(bw[x,y])
-            attributes.append(pix_row)
+        # Plot it!
+        #exp_var = pca_r.explained_variance_ratio_ * 64
+        #cum_exp_var = np.cumsum(exp_var)
+        # plt.figure(figsize=[7, 10])
+        # plt.bar(range(0, 64), exp_var, align='center', label='variance')
+        # plt.xlabel('Principal component index')
+        # plt.ylabel('Variance percentage')
+        # # plt.step(range(0, 64), cum_exp_var, where='mid', label='Cumulative explained variance', color='red')
+        # plt.show()
+
+        num = 32
+        pca_r = PCA(n_components=num)
+        pca_r_trans = pca_r.fit_transform(r_scaled)
+
+        pca_g = PCA(n_components=num)
+        pca_g_trans = pca_g.fit_transform(g_scaled)
+
+        pca_b = PCA(n_components=num)
+        pca_b_trans = pca_b.fit_transform(b_scaled)
+
+        # For calculating total loss of image quality.
+        # print("Explained variances by each channel")
+        # print("-----------------------------------")
+        # print("Red:", np.sum(pca_r.explained_variance_ratio_) * 100)
+        # print("Green:", np.sum(pca_g.explained_variance_ratio_) * 100)
+        # print("Blue:", np.sum(pca_b.explained_variance_ratio_) * 100)
+
+        pca_r_original = pca_r.inverse_transform(pca_r_trans)
+        pca_g_original = pca_g.inverse_transform(pca_g_trans)
+        pca_b_original = pca_b.inverse_transform(pca_b_trans)
+
+        compressed_image = cv2.merge((pca_b_original, pca_g_original, pca_r_original))
+        plt.figure(figsize=[2, 2])
+        plt.imshow(compressed_image)
+        plt.show()
