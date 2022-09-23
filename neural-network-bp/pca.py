e9eaea9f630d3f4f307a167e21ce72257e48e4be
Viktor Rydsund <v.rydsund@gmail.com>
2022-09-23 12:49:36 +0200

    Add feature extraction

--- neural-network-bp/pca.py
+++ neural-network-bp/pca.py
@@ -1,17 +1,16 @@
+import csv
 from os import listdir
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from sklearn.decomposition import PCA
 
 # Waldo
-path = '/home/vic/Downloads/archive/wheres-waldo/Hey-Waldo/64/waldo'
-count = 0
-for file in listdir(path):
-    if count < 1:
-        count += 1
+path = 'C:/Users/Vryds/Documents/wheres-waldo/Hey-Waldo/64/waldo'
+with open('features.csv', 'w') as f:
+    for file in listdir(path):
         image_path = path + '/' + file
-
         rgb_image = plt.imread(image_path)
         b, g, r = cv2.split(rgb_image)  # RGB are stored in reverse order in OpenCV
         r_scaled = r / 255
@@ -32,7 +31,8 @@ for file in listdir(path):
         # # plt.step(range(0, 64), cum_exp_var, where='mid', label='Cumulative explained variance', color='red')
         # plt.show()
 
-        num = 32
+
+        num = 32 # 32, ca 8% loss, 16 ca 45% loss.
         pca_r = PCA(n_components=num)
         pca_r_trans = pca_r.fit_transform(r_scaled)
 
@@ -42,6 +42,17 @@ for file in listdir(path):
         pca_b = PCA(n_components=num)
         pca_b_trans = pca_b.fit_transform(b_scaled)
 
+        feature_vec_r = numpy.asarray(pca_r_trans).flatten()
+        feature_vec_g = numpy.asarray(pca_g_trans).flatten()
+        feature_vec_b = numpy.asarray(pca_b_trans).flatten()
+
+        feature_vec = []
+        feature_vec = numpy.append(feature_vec, feature_vec_r, 0)
+        feature_vec = numpy.append(feature_vec, feature_vec_g, 0)
+        feature_vec = numpy.append(feature_vec, feature_vec_b, 0)
+
+        numpy.savetxt(f, feature_vec.reshape(1, feature_vec.shape[0]), delimiter=',',)
+
         # For calculating total loss of image quality.
         # print("Explained variances by each channel")
         # print("-----------------------------------")
@@ -49,11 +60,11 @@ for file in listdir(path):
         # print("Green:", np.sum(pca_g.explained_variance_ratio_) * 100)
         # print("Blue:", np.sum(pca_b.explained_variance_ratio_) * 100)
 
-        pca_r_original = pca_r.inverse_transform(pca_r_trans)
-        pca_g_original = pca_g.inverse_transform(pca_g_trans)
-        pca_b_original = pca_b.inverse_transform(pca_b_trans)
+        # pca_r_original = pca_r.inverse_transform(pca_r_trans)
+        # pca_g_original = pca_g.inverse_transform(pca_g_trans)
+        # pca_b_original = pca_b.inverse_transform(pca_b_trans)
 
-        compressed_image = cv2.merge((pca_b_original, pca_g_original, pca_r_original))
-        plt.figure(figsize=[2, 2])
-        plt.imshow(compressed_image)
-        plt.show()
+        #compressed_image = cv2.merge((pca_b_original, pca_g_original, pca_r_original))
+        #plt.figure(figsize=[2, 2])
+        #plt.imshow(compressed_image)
+        #plt.show()
