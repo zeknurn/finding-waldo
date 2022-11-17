eb2a7e1b9d645fc0635b2de7e4564db6f5c36260
Viktor Rydsund <v.rydsund@gmail.com>
2022-11-17 12:23:06 +0100

    Small changes to naive bayes

--- bayesian-classifier/bayesian_classifier.py
+++ bayesian-classifier/bayesian_classifier.py
@@ -1,14 +1,22 @@
 # Naive Bayesian Classifier.
+import feature_extraction
 
 # Prior probability of seeing waldo, i.e. the ratio of waldo images to non-waldo images in our training set.
 # Set arbitrarily to 20%
 prior_waldo = 0.2
 prior_not_waldo = 1 - prior_waldo
 
+glcm_waldo = 0.9997621193910255
+avg_glcm_waldo = 0.0038158859518741435
+glcm_notwaldo = 1.007062237607153
+avg_glcm_notwaldo = 0.003843748998500584
+
 # Probability of containing waldo, i.e. a waldo specific feature, given the image we are looking at contains waldo, i.e:
 # P(Glasses | Waldo)
 
 # The glcm score of the image, divided by the total avg. gclm score of all waldo images.
+# The the times money appear in the spam message, divided by all the words in the spam message.
+# the glcm value, divided by all the glcm values in the waldo image. p(glcm | waldo)
 
 # Probability of the same feature given that the image we are looking at does not contain waldo:
 # P(Glasses | Not Waldo)
@@ -22,12 +30,3 @@ prior_not_waldo = 1 - prior_waldo
 
 # We then need to look at each image individually and determine the probability of each feature:
 # given Waldo, and not Waldo.
-
-# Then we calculate the score for that image, and compare our result with the actual answer.
-# with open('probabilities_waldo.csv', 'r') as in_waldo:
-#     with open('probabilities_notwaldo_preprocessed.csv', 'w') as out_waldo:
-#         for line in in_waldo:
-#             for value in line:
-#                 if value != 0:
-#                     out_waldo.write(line)
-
