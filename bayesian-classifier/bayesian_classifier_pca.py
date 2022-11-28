d314cf703c743f91fb692e6e2f445ed020b0f6af
Viktor Rydsund <v.rydsund@gmail.com>
2022-11-28 15:56:20 +0100

    Working but broken CDF Bayesian

--- bayesian-classifier/bayesian_classifier_pca.py
+++ bayesian-classifier/bayesian_classifier_pca.py
@@ -6,16 +6,14 @@ from numpy import mean
 from numpy import std
 
 
-def load_waldo_data(nr, p):
-    A = int(nr * (1 - p))  # Waldo
-    B = int(nr * p)  # Not Waldo
+def load_waldo_data(nr):
     print("loading data")
     # Read data file and label images with Waldo as 1, and not Waldo as 0.
     with open('features_waldo.csv', 'r') as f:
-        x = np.loadtxt(f, delimiter=',', max_rows=A)
+        x = np.loadtxt(f, delimiter=',', max_rows=nr)
         y = np.ones(shape=(x.shape[0], 1))
     with open('features_notwaldo.csv', 'r') as t:
-        x_2 = np.loadtxt(t, delimiter=',', max_rows=B)
+        x_2 = np.loadtxt(t, delimiter=',', max_rows=nr)
         y_2 = np.zeros(shape=(x_2.shape[0], 1))
     x = np.append(x, x_2, axis=0)
     y = np.append(y, y_2, axis=0)
@@ -51,56 +49,97 @@ def load_waldo_data(nr, p):
 
 
 # fit a probability distribution to a univariate data sample
-def fit_distribution(data):
-    # estimate parameters
-    mu = mean(data)
-    sigma = std(data)
-    print(mu, sigma)
-    # fit distribution
-    dist = norm(mu, sigma)
-    return dist
 
 
 # calculate the independent conditional probability
-def probability(X, prior, dist1, dist2):
-    return prior * dist1.pdf(X[0]) * dist2.pdf(X[1])
 
 
-def probability_new(X, prior, distributions):
-    score = 0
-    for i in range(len(distributions)):
-        score *= distributions[i].pdf(X[i])
-    return prior * score
-
-
-# Prior is the ratio of not Waldo to Waldo
-nr_data_points = 100
-prior = 0.8
+nr_data_points = 10
 
 # Load data
 X_example, y_example = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1)
-X, y = load_waldo_data(nr_data_points, prior)
+X, y = load_waldo_data(nr_data_points)
 y = np.ndarray.flatten(y)
 y = y.astype(int)
-print('Our data shape: ', X.shape, '/// Example data shape: ', X_example.shape)
-print('Our data shape: ', y.shape, '/// Example data shape: ', y_example.shape)
+print('Waldo shape X: ', X.shape, '/// Example shape X: ', X_example.shape)
+print('Waldo shape y: ', y.shape, '/// Example shape y: ', y_example.shape)
 print('Waldo dataset:')
 print(X[:2], y[:2])
-print('Blobs datasetD:')
+print('Blobs dataset:')
 print(X_example[:2], y_example[:2])
 
 # # sort data into classes
 Xy0 = X[y == 0]
 Xy1 = X[y == 1]
+print('Sort data into classes')
+print(Xy0.shape, Xy1.shape)
 
-# print(X[:5])
-# print(y[:5])
-# print(y)
-# Priors
-# prior_w = len()
+
+def fit_distribution(data):
+    # estimate parameters
+    mu = mean(data)
+    sigma = std(data)
+    # print(mu, sigma)
+    # fit distribution
+    dist = norm(mu, sigma)
+    return dist
+
+
+def probability(X, prior, dist1, dist2):
+    return prior * dist1.pdf(X[0]) * dist2.pdf(X[1])
+
+
+# If underflow possible, convert multiplication to log.
+def probability(X, prior, distributions):
+    for i in range(0, 32):
+        prior *= np.log2(distributions[i].pdf(X[i]))
+        # print('Thinking...', prior, i)
+
+    return prior
+
+
+# Loop, rad N. 6144
+# Create PDFs for y == 0
+X1y0 = fit_distribution(Xy0[:, 0])
+X2y0 = fit_distribution(Xy0[:, 1])
+
+# Distributions for y == 0
+dist0 = np.empty(Xy0.shape[1], dtype=type(X1y0))
+for i in range(0, Xy0.shape[1]):
+    dist0[i] = fit_distribution(Xy0[:, i])
+
+# Create PDfs for y == 1
+X1y1 = fit_distribution(Xy1[:, 0])
+X2y1 = fit_distribution(Xy1[:, 1])
+
+# Distributions for y == 1
+dist1 = np.empty(Xy1.shape[1], dtype=type(X1y0))
+for i in range(0, Xy1.shape[1]):
+    dist1[i] = fit_distribution(Xy1[:, i])
+
+priory0 = len(Xy0) / len(X)
+priory1 = len(Xy1) / len(X)
+
+score = 0
+# classify one example
+print(nr_data_points)
+for i in range(nr_data_points):
+    Xsample, ysample = X[i], y[i]
+    # py0 = probability(Xsample, priory0, X1y0, X2y0) # given waldo
+    # py1 = probability(Xsample, priory1, X1y1, X2y1) # given not waldo
+    py0 = probability(Xsample, priory0, dist0)  # Cumulative probability given not Waldo
+    py1 = probability(Xsample, priory1, dist1)  # CDF probability given not Waldo
+    print('P(y=0 | %s) = %.3f' % (Xsample, py0 * 100))
+    print('P(y=1 | %s) = %.3f' % (Xsample, py1 * 100))
+    if py0 > py1 and y[i] == 0:
+        score += 1
+    elif py1 > py0 and y[i] == 1:
+        score += 1
+    print('Truth: y=%d' % ysample)
+
+print(score / nr_data_points)
 
 # # generate 2d classification dataset
-
 
 # print(Xy0.shape, Xy1.shape)
 # print(Xy1)
@@ -113,8 +152,7 @@ Xy1 = X[y == 1]
 # # create PDFs for y==1
 # distX1y1 = fit_distribution(Xy1[:, 0])
 # distX2y1 = fit_distribution(Xy1[:, 1])
-# # classify one example
-# Xsample, ysample = X[0], y[0]
+
 # py0 = probability(Xsample, priory0, distX1y0, distX2y0)
 # py1 = probability(Xsample, priory1, distX1y1, distX2y1)
 # print('P(y=0 | %s) = %.3f' % (Xsample, py0 * 100))
