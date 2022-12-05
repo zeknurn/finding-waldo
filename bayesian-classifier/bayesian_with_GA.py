d2942bc35be5436dfc24966d3e59451052c36afe
Viktor Rydsund <v.rydsund@gmail.com>
2022-12-05 12:43:04 +0100

    Clean up some code, add new file.
    Added some comments on sections of code. Also created a new file to start implementation of GA.

--- bayesian-classifier/bayesian_with_GA.py
+++ bayesian-classifier/bayesian_with_GA.py
@@ -35,19 +35,12 @@ def load_waldo_data(nr):
 
 nr_data_points = 2
 # Load data
-# X_example, y_example = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1)
 X, y = load_waldo_data(nr_data_points)
 y = np.ndarray.flatten(y)
 y = y.astype(int)
-# print('Waldo shape X: ', X.shape, '/// Example shape X: ', X_example.shape)
-# print('Waldo shape y: ', y.shape, '/// Example shape y: ', y_example.shape)
 print('Waldo dataset:')
 print('Waldo shape X:', X.shape)
 print('Waldo shape y:', y.shape)
-
-# print(X[:2], y[:2])
-# print('Blobs dataset:')
-# print(X_example[:2], y_example[:2])
 
 # # sort data into classes
 Xy0 = X[y == 0]
@@ -64,20 +57,6 @@ def fit_distribution(data):
     dist = norm(mu, sigma)
     return dist
 
-
-def probability(X, prior, dist1, dist2):
-    return prior * dist1.pdf(X[0]) * dist2.pdf(X[1])
-# If underflow possible, convert multiplication to log.
-# def probability(X, prior, distributions):
-#     # prior = 1
-#     c = np.empty(distributions.shape[0])
-#
-#     for i in range(0, 32):
-#    # for i in range(0, distributions.shape[0]):
-#         value = distributions[i].pdf(X[i])
-#         c[i] = value
-#         prior *= value
-#     return prior
 
 # The Bayesian probability function.
 # Here we use the values obtained from the probability density function of each variable.
@@ -96,21 +75,14 @@ def logsumexp(x):
     return c + np.log(np.sum(np.exp(x - c)))
 
 
-# Loop, rad N. 6144
-# Create PDFs for y == 0
 start_time = time.time()
-
+# Done for dtype.
 X1y0 = fit_distribution(Xy0[:, 0])
-# X2y0 = fit_distribution(Xy0[:, 1])
 
 # Distributions for y == 0
 dist0 = np.empty(Xy0.shape[1], dtype=type(X1y0))
 for i in range(0, Xy0.shape[1]):
     dist0[i] = fit_distribution(Xy0[:, i])
-
-# Create PDfs for y == 1
-# X1y1 = fit_distribution(Xy1[:, 0])
-# X2y1 = fit_distribution(Xy1[:, 1])
 
 # Distributions for y == 1
 dist1 = np.empty(Xy1.shape[1], dtype=type(X1y0))
@@ -120,27 +92,33 @@ for i in range(0, Xy1.shape[1]):
 priory0 = len(Xy0) / len(X)
 priory1 = len(Xy1) / len(X)
 
+# Apply GA
+# Representation of distributions
+# A fitness function
+# recombination
+# mutation
+
 score = 0
 # classify one example
-print(nr_data_points)
-for i in range(nr_data_points):
-    Xsample, ysample = X[i], y[i]  # en rad
-    # py0 = probability(Xsample, priory0, X1y0, X2y0) # given not Waldo
-    # py1 = probability(Xsample, priory1, X1y1, X2y1) # given Waldo
-    log_arr0 = probability(Xsample, priory0, dist0)  # Cumulative probability given not Waldo
-    log_arr1 = probability(Xsample, priory1, dist1)  # CDF probability given not Waldo
-
-    # Test
-    py0 = priory0 * logsumexp(log_arr0)
-    py1 = priory1 * logsumexp(log_arr1)
-
-    print('P(y=0 | %s) = %.3f' % (Xsample, py0 * 100))
-    print('P(y=1 | %s) = %.3f' % (Xsample, py1 * 100))
-    if py0 > py1 and y[i] == 0:
-        score += 1
-    elif py1 > py0 and y[i] == 1:
-        score += 1
-    print('Truth: y=%d' % ysample)
-
-print("--- %s seconds ---" % (time.time() - start_time))
-print(score / nr_data_points)
+# print(nr_data_points)
+# for i in range(nr_data_points):
+#     Xsample, ysample = X[i], y[i]  # en rad
+#     # py0 = probability(Xsample, priory0, X1y0, X2y0) # given not Waldo
+#     # py1 = probability(Xsample, priory1, X1y1, X2y1) # given Waldo
+#     log_arr0 = probability(Xsample, priory0, dist0)  # Cumulative probability given not Waldo
+#     log_arr1 = probability(Xsample, priory1, dist1)  # CDF probability given not Waldo
+#
+#     # Test
+#     py0 = priory0 * logsumexp(log_arr0)
+#     py1 = priory1 * logsumexp(log_arr1)
+#
+#     print('P(y=0 | %s) = %.3f' % (Xsample, py0 * 100))
+#     print('P(y=1 | %s) = %.3f' % (Xsample, py1 * 100))
+#     if py0 > py1 and y[i] == 0:
+#         score += 1
+#     elif py1 > py0 and y[i] == 1:
+#         score += 1
+#     print('Truth: y=%d' % ysample)
+#
+# print("--- %s seconds ---" % (time.time() - start_time))
+# print(score / nr_data_points)
