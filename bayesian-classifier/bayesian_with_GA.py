ee8384687a3373a91e030bf4e7c26bd33ff33906
Viktor Rydsund <v.rydsund@gmail.com>
2022-12-05 14:52:04 +0100

    Add first draft of GA

--- bayesian-classifier/bayesian_with_GA.py
+++ bayesian-classifier/bayesian_with_GA.py
@@ -1,4 +1,8 @@
 # example of preparing and making a prediction with a naive bayes model
+import collections
+import random
+
+import numpy.random
 from sklearn.datasets import make_blobs
 from scipy.stats import norm
 import numpy as np
@@ -33,20 +37,22 @@ def load_waldo_data(nr):
     return x, y
 
 
-nr_data_points = 2
+nr_data_points = 6
 # Load data
 X, y = load_waldo_data(nr_data_points)
 y = np.ndarray.flatten(y)
 y = y.astype(int)
-print('Waldo dataset:')
-print('Waldo shape X:', X.shape)
-print('Waldo shape y:', y.shape)
+# print('Waldo dataset:')
+# print('Waldo shape X:', X.shape)
+# print('Waldo shape y:', y.shape)
 
-# # sort data into classes
+# sort data into classes
 Xy0 = X[y == 0]
 Xy1 = X[y == 1]
-print('Sort data into classes')
-print(Xy0.shape, Xy1.shape)
+
+
+# print('Sort data into classes')
+# print(Xy0.shape, Xy1.shape)
 
 
 # This function fits each and every single variable in a column to a normal distribution.
@@ -70,15 +76,63 @@ def probability(X, prior, distributions):
     return log_arr
 
 
+# This probability function uses indexes from GA to find which PDF values we should use.
+# Works identically as the one above, except different indexes.
+def probability_ga(X, prior, distributions, population):
+    log_arr = np.empty(distributions.shape[0])
+    for j in range(0, distributions.shape[0]):
+        log_arr[j] = distributions[population[j]].pdf(X[j])
+    return log_arr
+
+
 def logsumexp(x):
     c = x.max()
     return c + np.log(np.sum(np.exp(x - c)))
 
 
-start_time = time.time()
+def fitness(Xsample, ysample, population):
+    fitness_score = 0
+    log_arr0 = probability_ga(Xsample, priory0, dist0, population)  # Cumulative probability given not Waldo
+    log_arr1 = probability_ga(Xsample, priory1, dist1, population)  # CDF probability given not Waldo
+
+    # Test
+    py0 = priory0 * logsumexp(log_arr0)
+    py1 = priory1 * logsumexp(log_arr1)
+
+    print('P(y=0 | %s) = %.3f' % (Xsample, py0))
+    print('P(y=1 | %s) = %.3f' % (Xsample, py1))
+    print('Truth: y=%d' % ysample)
+    if ysample == 0:
+        fitness_score = py0 - py1
+    if ysample == 1:
+        fitness_score = py1 - py0
+    return fitness_score
+
+
+def rank_fitness():
+    pop_score = {}
+    for i in range(0, 5):
+        score = 0
+        for j in range(0, nr_data_points):
+            score += fitness(X[j], y[j], populations[i])
+        pop_score[i] = score
+    sorted_pop_score = collections.OrderedDict(pop_score)
+    return sorted_pop_score
+
+def recombination(sorted_dict):
+    a, b = sorted_dict[:2]
+    print(a, b)
+    # popA = populations[a]
+    # popB = populations[b]
+    # recombination, 80-90%
+    # copy, 10-20%
+
+
 # Done for dtype.
 X1y0 = fit_distribution(Xy0[:, 0])
 
+# 6144
+# One distribution for each colum
 # Distributions for y == 0
 dist0 = np.empty(Xy0.shape[1], dtype=type(X1y0))
 for i in range(0, Xy0.shape[1]):
@@ -94,31 +148,23 @@ priory1 = len(Xy1) / len(X)
 
 # Apply GA
 # Representation of distributions
-# A fitness function
-# recombination
-# mutation
+population_count = 100
+populations = []
+pop0 = np.concatenate([np.arange(i, 6144) for i in range(0, 6144)])
+np.random.seed(1337)
+for i in range(0, population_count):
+    np.random.shuffle(pop0)
+    populations.append(pop0)
 
-score = 0
-# classify one example
-# print(nr_data_points)
-# for i in range(nr_data_points):
-#     Xsample, ysample = X[i], y[i]  # en rad
-#     # py0 = probability(Xsample, priory0, X1y0, X2y0) # given not Waldo
-#     # py1 = probability(Xsample, priory1, X1y1, X2y1) # given Waldo
-#     log_arr0 = probability(Xsample, priory0, dist0)  # Cumulative probability given not Waldo
-#     log_arr1 = probability(Xsample, priory1, dist1)  # CDF probability given not Waldo
-#
-#     # Test
-#     py0 = priory0 * logsumexp(log_arr0)
-#     py1 = priory1 * logsumexp(log_arr1)
-#
-#     print('P(y=0 | %s) = %.3f' % (Xsample, py0 * 100))
-#     print('P(y=1 | %s) = %.3f' % (Xsample, py1 * 100))
-#     if py0 > py1 and y[i] == 0:
-#         score += 1
-#     elif py1 > py0 and y[i] == 1:
-#         score += 1
-#     print('Truth: y=%d' % ysample)
-#
-# print("--- %s seconds ---" % (time.time() - start_time))
-# print(score / nr_data_points)
+sorted_dict = rank_fitness()
+print(sorted_dict)
+recombination(sorted_dict)
+
+
+
+
+# Fitness calculation
+# X[i], y[i]
+
+
+# mutation, 1/L
