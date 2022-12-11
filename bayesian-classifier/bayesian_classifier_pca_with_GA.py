ecbb3d5fdd30d4e4ec38e92e337dfa91f84cdd57
Johan A <johanandersson@outlook.com>
2022-12-11 16:44:47 +0100

    cleaned up and added comments to bayesian classifier with GA

--- bayesian-classifier/bayesian_classifier_pca_with_GA.py
+++ bayesian-classifier/bayesian_classifier_pca_with_GA.py
@@ -25,20 +25,24 @@ def probability_ga(Xsample, distributions, population):
     return log_arr
 
 
-def calculate_fitness_score(Xsample, ysample, population):
+# calcualtes the fitness score of the given sample using the label and the poplation
+def calculate_fitness_score(Xsample, ysample, population, priory0, priory1, dist0, dist1):
     fitness_score = 0
     log_arr0 = priory0 * probability_ga(Xsample, dist0, population)  # Cumulative probability given not Waldo
     log_arr1 = priory1 * probability_ga(Xsample, dist1, population)  # CDF probability given not Waldo
 
-    # Note: Possibly remove prior for GA testing,
     py0, py1 = bayesian_classifier_pca.logsumexp(log_arr0, log_arr1)
 
-    # necessary bias
-    #py1 -= 0.1
+    # necessary bias relative to the size of the sample. 
+    # bigger sample needs a bigger bias. for 100 samples 1 is good, for 10 samples 0.1 works
+    py1 -= 0.1
 
     # print('P(y=0 | %s) = %.3f' % (Xsample, py0))
     # print('P(y=1 | %s) = %.3f' % (Xsample, py1))
     # print('Truth: y=%d' % ysample)
+
+    # calculate the fitness by finding on the difference in prediction depending on the truth
+    # the bigger the difference the better
     if ysample == 0:
         fitness_score = py0 - py1
     if ysample == 1:
@@ -48,10 +52,10 @@ def calculate_fitness_score(Xsample, ysample, population):
 
 # Calculates fitness for each population, and replaces the previous score obtained from crossover.
 # Then the entire population is sorted, ranked, by fitness score.
-def rank_fitness(populations, best_score_current):
+def rank_fitness(populations, best_score_current, sample_size, X, y, priory0, priory1, dist0, dist1):
     for i in range(0, len(populations)):
         print("Ranking pop: ", i)
-        populations[i] = populations[i][0], populations[i][1], cumulative_fitness(populations[i][1])
+        populations[i] = populations[i][0], populations[i][1], cumulative_fitness(populations[i][1], sample_size, X, y, priory0, priory1, dist0, dist1)
     populations = sorted(populations, key=lambda x: x[2], reverse=True)
 
     best_score_new = populations[0][2]
@@ -63,15 +67,18 @@ def rank_fitness(populations, best_score_current):
     return populations, best_score_new
 
 
-def cumulative_fitness(population):
+# calculates cumulative fitness for a population
+def cumulative_fitness(population, sample_size, X, y, priory0, priory1, dist0, dist1):
     tmp = 0
     for j in range(0, sample_size):
         print("Calculating fitness for data point nr: ", j)
-        tmp += calculate_fitness_score(X[j], y[j], population)
+        tmp += calculate_fitness_score(X[j], y[j], population, priory0, priory1, dist0, dist1)
 
     return tmp
 
 
+# performs crossover on two pots by taking the first half from the first pop and combining it with
+# the second half of the second pop and vice versa.
 def crossover_inner(p1, p2):
     split = int(len(p1) / 2)
     c1 = p1[:split]
@@ -80,34 +87,34 @@ def crossover_inner(p1, p2):
     c2 = np.append(c2, p1[split:])
     return c1, c2
 
-
-# Crossover populations
-# def crossover_outer(populations):
-#     for i in range(0, len(populations), 2):
-#         p1 = populations[i][1]  # Values
-#         p2 = populations[i + 1][1]
-#         c1, c2 = crossover_inner(p1, p2)
-#         populations[i] = populations[i][0], c1, 0 #populations[i][2]
-#         populations[i + 1] = populations[i + 1][0], c2, 0 # populations[i + 1][2]
-#     return populations
-
+# performs pair wise crossover where the top pop in top half is crossedover with the top pop in bottom half.
+# and so on.
 def crossover_outer(populations):
     step = int(len(populations) / 2)
     for i in range(0, step):
         p1 = populations[i][1]  # Values
         p2 = populations[i + step][1]
         c1, c2 = crossover_inner(p1, p2)
-        populations[i] = populations[i][0], c1, 0  # populations[i][2]
-        populations[i + step] = populations[i + step][0], c2, 0  # populations[i + 1][2]
+
+        # copy index, add new pop sequence and reset fitness score
+        populations[i] = populations[i][0], c1, 0 
+        # copy index, add new pop sequence and reset fitness score
+        populations[i + step] = populations[i + step][0], c2, 0 
+
     return populations
 
 
 # Apply mutation
 # Selects a population from populations randomly
 # Thereafter select both an index and value which is randomly selected and replaced with a random value within range.
-def mutate(populations):
+def mutate(populations, population_count):
+    # gets the indexes of one tenth of the population
     mut_sample_index = np.random.choice(population_count, 10)
+
+    # gets 10 indexes of the location in a pop for mutation
     mut_pop_sequence_index = np.random.choice(6144, 10)
+
+    # gets 10 values that will be placed on the indexes in each pop
     mut_value = np.random.choice(6144, 10)
 
     for index in mut_sample_index:
@@ -121,7 +128,7 @@ def mutate(populations):
 # 0, index
 # 1 [123, 234, 35345, 342]
 # 2, fitness score
-def create_population():
+def create_population(population_count):
     populations = []
     starting_pop = np.arange(6144)
     for i in range(0, population_count):
@@ -132,8 +139,8 @@ def create_population():
     return populations
 
 
-def run_ga(epochs, best_score_current, populations, sample_size, results):
-
+# runs ga for the given amount of epochs and sample size over the populations
+def run_ga(epochs, best_score_current, populations, sample_size, results, X, y, priory0, priory1, dist0, dist1, population_count):
 
     for i in range(0, epochs):
         print('Epoch: ', i, ':')
@@ -143,83 +150,32 @@ def run_ga(epochs, best_score_current, populations, sample_size, results):
         print('Crossover done - Epoch: ', i)
         # print(populations)
         print('Starting mutation - Epoch: ', i)
-        populations = mutate(populations)
+        populations = mutate(populations, population_count)
         print('Mutation done - Epoch: ', i)
         # print(populations)
         print('Starting rank fitness - Epoch: ', i)
 
-        populations, best_score_new = rank_fitness(populations, best_score_current)
+        # rank and get best new fitness score from the batch
+        populations, best_score_new = rank_fitness(populations, best_score_current, sample_size, X, y, priory0, priory1, dist0, dist1)
+
+        # save best score for plotting
         new_stats = pd.DataFrame([(best_score_new)], columns=["best_score"])
         results = pd.concat([results, new_stats], ignore_index=True)
+
         if best_score_new > best_score_current:
             best_score_current = best_score_new
 
         print('Rank fitness done - Epoch: ', i)
         print(populations)
 
-    
+    # plot the best score over epochs
     plt.clf()
     plt.xlabel("Epochs")
     results.best_score.plot(title="Best Fitness Score")
     plt.savefig("bestscore_pop_size{}_samplesize{}_epochs{}.png".format(len(populations), sample_size, epochs))
 
-
-def init():
-    # Load data
-    # X_example, y_example = make_blobs(n_samples=50, centers=2, n_features=2, random_state=1)
-    X_sample, y_sample, X_all, y_all = bayesian_classifier_pca.load_waldo_data(sample_size)
-
-    y_sample = np.ndarray.flatten(y_sample)
-    y_sample = y_sample.astype(int)
-
-    y_all = np.ndarray.flatten(y_all)
-    y_all = y_all.astype(int)
-
-    # print('Waldo shape X: ', X.shape, '/// Example shape X: ', X_example.shape)
-    # print('Waldo shape y: ', y.shape, '/// Example shape y: ', y_example.shape)
-    print('Waldo dataset:')
-    print('Waldo shape X:', X_sample.shape)
-    print('Waldo shape y:', y_sample.shape)
-
-    # print(X[:2], y[:2])
-    # print('Blobs dataset:')
-    # print(X_example[:2], y_example[:2])
-
-    # # sort data into classes
-    #Xy0 = X_all[y_all == 0]
-    #Xy1 = X_all[y_all == 1]
-    Xy0 = X_sample[y_sample == 0]
-    Xy1 = X_sample[y_sample == 1]
-    print('Sort data into classes')
-    print("Not Waldo: ", Xy0.shape, "Waldo: ", Xy1.shape)
-
-    # Loop, rad N. 6144
-    # Create PDFs for y == 0
-
-    X1y0 = bayesian_classifier_pca.fit_distribution(Xy0[:, 0])
-    # X2y0 = fit_distribution(Xy0[:, 1])
-
-    # Distributions for y == 0
-    dist0 = np.empty(Xy0.shape[1], dtype=type(X1y0))
-    for i in range(0, Xy0.shape[1]):
-        dist0[i] = bayesian_classifier_pca.fit_distribution(Xy0[:, i])
-
-    # Create PDfs for y == 1
-    # X1y1 = fit_distribution(Xy1[:, 0])
-    # X2y1 = fit_distribution(Xy1[:, 1])
-
-    # Distributions for y == 1
-    dist1 = np.empty(Xy1.shape[1], dtype=type(X1y0))
-    for i in range(0, Xy1.shape[1]):
-        dist1[i] = bayesian_classifier_pca.fit_distribution(Xy1[:, i])
-
-    priory0 = len(Xy0) / len(X_all)
-    priory1 = len(Xy1) / len(X_all)
-
-    return X_sample, y_sample, dist0, dist1, priory0, priory1
-
-
-def classify():
+# classify the given data 
+def classify(X, y, dist0, dist1, priory0, priory1, sample_size, bias):
     print("Loading best pop from csv")
     best_population = np.loadtxt('ga_best_population.csv', delimiter=',').astype(np.int64)
 
@@ -237,8 +193,9 @@ def classify():
         # Note: Possibly remove prior for GA testing,
         py0, py1 = bayesian_classifier_pca.logsumexp(log_arr0, log_arr1)
 
-        # necessary bias
-        #py1 -= 0.1
+        # necessary bias relative to the size of the sample. 
+        # bigger sample needs a bigger bias. for 100 samples 1 is enough.
+        py1 -= bias
 
         print('Data point: ', i)
         print('P(y=0 | %s)' % py0)
@@ -274,40 +231,53 @@ def classify():
     print("false_negative: ", (false_negative / test_waldo_count * 100, "%"))
 
 
+
 # Apply GA #########################
 # Representation of distributions.
 # The population is represented by an array of indexes, each index is a key for a PDF.
 # The GA works by finding a combination of PDFs that yield the highest score.
-population_count = 2
-sample_size = 10
-epochs = 2
-start_time = time.time()
+def main():
+    population_count = 2
+    sample_size = 4
+    epochs = 2
 
-np.random.seed(1337)  # Reproducible results
-X, y, dist0, dist1, priory0, priory1 = init()
+    # necessary bias relative to the size of the sample. 
+    # bigger sample needs a bigger bias. for 100 samples 1 is enough. for 10 samples 0.1 is enough
+    # set to 0 to try to use GA to remove the need for the bias
+    bias = 0
 
-print('Creating starting population')
-populations = create_population()
-print("Ranking fitness of starting population")
+    start_time = time.time()
 
-# create dataframe to store results for plotting
-results = pd.DataFrame(columns=["best_score"])
+    # Set random seed for reproducible results 
+    np.random.seed(1337)  
 
-# rank and get the best score of the starting pop
-populations, best_score_current = rank_fitness(populations, 0)
+    # load data and get dists and priors
+    X, y, dist0, dist1, priory0, priory1 = bayesian_classifier_pca.init(sample_size)
 
-# add the best score of the starting pop
-new_stats = pd.DataFrame([(best_score_current)], columns=["best_score"])
-results = pd.concat([results, new_stats], ignore_index=True)
-print("Best starting pop score: ", best_score_current)
+    print('Creating starting population')
+    populations = create_population(population_count)
+    print("Ranking fitness of starting population")
 
-# classify using starting pop
-classify()
+    # create dataframe to store results for plotting
+    results = pd.DataFrame(columns=["best_score"])
 
-# run ga on the populations
-run_ga(epochs, best_score_current, populations, sample_size, results)
+    # rank and get the best score of the starting pop
+    populations, best_score_current = rank_fitness(populations, 0, sample_size, X, y, priory0, priory1, dist0, dist1)
 
-# classify using the best evolved pop
-classify()
+    # add the best score of the starting pop
+    new_stats = pd.DataFrame([(best_score_current)], columns=["best_score"])
+    results = pd.concat([results, new_stats], ignore_index=True)
+    print("Best starting pop score: ", best_score_current)
 
-print("--- %s seconds ---" % (time.time() - start_time))
+    # classify using starting pop
+    classify(X, y, dist0, dist1, priory0, priory1, sample_size, bias)
+
+    # run ga on the populations
+    run_ga(epochs, best_score_current, populations, sample_size, results, X, y, priory0, priory1, dist0, dist1, population_count)
+
+    # classify using the best evolved pop
+    classify(X, y, dist0, dist1, priory0, priory1, sample_size, bias)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+main()
